dccad0b57bf57d44d159ad5c3c06821644bae48c
Denali Stevens <denalistevens01@gmail.com>
2024-04-14 22:30:45 -0400

    I was able to create a data set that I will eventually be able to plot on my map and then click on parks to get the information I scraped wiki for, but I cannot get it to plot right now. I think it has something to do with the file type. I also added tabs to my shiny app and started what I want the next tab to be. I'm using a new data set to plot visitors.

--- Final_App/app.R
+++ Final_App/app.R
@@ -4,6 +4,9 @@ library(sf)
 library(here)
 library(tidyverse)
 library(leaflet)
+library(plotly)
+library(scales)
+options(scipen = 999)
 
 # Bring in data and manipulate 
 nps <- sf::read_sf("/Users/denalistevens/Desktop/Spring 2024/SYE/SYE/NPS_shape/nps_boundary.shp") |>
@@ -12,17 +15,44 @@ p_names <- nps|>
   mutate(popup = paste0(nps$UNIT_NAME))
 
 
-ui <- fluidPage(
-  h1("National Park Map"),
-  hr("This is data collected from the National Park Service to display the boundaries of national parks 
-     both federally and non-federally owned"),
-  sidebarLayout(
-    sidebarPanel(),
-    mainPanel(
-      leafletOutput("map1")
-              )
-  )
-)
+library("httr")
+library("readxl")
+GET("https://query.data.world/s/fr5k6pcrcweo7wr657vchbeq6f3cci?dws=00000", write_disk(tf <- tempfile(fileext = ".xlsx")))
+df <- read_excel(tf)
+years <- unique(df$YearRaw) 
+type <- unique(df$`Unit Type`)
+national_parks3 <- national_parks |> filter(YearRaw != "Total") |> filter(Visitors >= 1000000)
+
+ui <- navbarPage("NavBar!",
+                 tabPanel("Map",
+                          h1("National Park Map"),
+                          hr("This is data collected from the National Park Service to 
+                          display the boundaries of national parks 
+                          both federally and non-federally owned"),
+                          leafletOutput("map1")),
+                 tabPanel("Visitors Plot",
+                          h1("Number of Visitors from 1904 - 2016"),
+                          sidebarLayout(
+                            sidebarPanel(
+                              sliderInput("years_select", label = "Set Years for Graph",
+                                          min = 1904,
+                                          max = 2016,
+                                          value = c(2000, 2016)),
+                              # need to find a way to get rid of commas so it looks like years
+                              selectInput("type_select", label = "Select Type to Graph",
+                                          choices = type, multiple = TRUE),
+                              sliderInput("min_visitors", label = "Set a Minimum Amount of Visitors",
+                                          min = 0,
+                                          max = 3000000,
+                                          value = 1000000),
+                            ),
+                              mainPanel(
+                                plotOutput("visitors_line")
+                        
+                              )
+                            )
+                          ))
+
 
 
 server <- function(input, output) {
@@ -42,6 +72,18 @@ server <- function(input, output) {
                     popup = p_names$popup)
         
     })
+    
+    output$visitors_line <- renderPlot({
+      ggplot(data = national_parks3, aes(x = YearRaw, y = Visitors, group = `Unit Name`)) +
+        geom_line(aes(colour = `Unit Name`)) +
+        scale_color_viridis_d() +
+        scale_y_continuous(labels = scales::comma) +
+        labs(title = "Visitors in National Parks from 2000 to 2016", subtitle = "Minimum visitors is set at 1,000,000") +
+        theme_minimal()
+     
+    })
+    
+    
 }
 
 
