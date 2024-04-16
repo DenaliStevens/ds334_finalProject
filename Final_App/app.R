6c5c801bbc957f4464db613d9d181ba3f68ca4c2
Denali Stevens <denalistevens01@gmail.com>
2024-04-16 12:55:46 -0400

    Added leaflet updates to my app and got as far as I could with the map. Also am working with my visitors plot to make it interactive. Currently where I am stuck.

--- Final_App/app.R
+++ Final_App/app.R
@@ -6,6 +6,13 @@ library(tidyverse)
 library(leaflet)
 library(plotly)
 library(scales)
+library(rvest)
+library(stringr)
+library(htmltools)
+library(glue)
+library("httr")
+library("readxl")
+library(plotly)
 options(scipen = 999)
 
 # Bring in data and manipulate 
@@ -14,14 +21,55 @@ nps <- sf::read_sf("/Users/denalistevens/Desktop/Spring 2024/SYE/SYE/NPS_shape/n
 p_names <- nps|>
   mutate(popup = paste0(nps$UNIT_NAME))
 
+# need to make leaflet plot 
+url <- "https://en.wikipedia.org/wiki/List_of_national_parks_of_the_United_States"
+h <- read_html(url)
+tab <- h |> html_nodes("table")
+wiki_np_data <- tab[[1]] |> html_table()
 
-library("httr")
-library("readxl")
+wiki_np_clean <- wiki_np_data |> mutate(wiki_name = str_replace_all(Name, "[^[:alnum:]]", " ")) |>
+  relocate(wiki_name)
+nps_clean <- nps |> mutate(nps_name = str_remove_all(UNIT_NAME, " National") |>
+                             str_remove_all(" Historical Site") |> 
+                             str_remove_all( " Historical Park") |>
+                             str_remove_all( " Historical Trail") |>
+                             str_remove_all( " Park")) |>
+  relocate(nps_name)
+
+joined2 <- nps_clean |>
+  stringdist_inner_join(wiki_np_clean, by = c("nps_name" = "wiki_name"), max_dist = 3) |>
+  relocate(wiki_name, nps_name)
+names_2 <- joined2 |>
+  mutate(popup = paste0('<a href =', joined2$METADATA, '>',
+                        joined2$UNIT_NAME, '</a>'))
+joined_clean <- joined2 |> rename(Established = `Date established as park[12]`, Area = `Area (2023)[8]`, Visitors_2022 = `Recreation visitors (2022)[11]`) 
+
+joined_clean <- joined_clean |> mutate(popup = paste(
+  "Name: ", UNIT_NAME,
+  "Date of Establishment: ", Established,
+  "Visitors in 2022: ", Visitors_2022,
+  "Description of Park: ", Description))
+label_text <- glue(
+  "<b> Name: </b> {joined_clean$UNIT_NAME}<br/>",
+  "<b> Established: </b> {joined_clean$Established}<br/>",
+  "<b> Visitors in 2022: </b> {joined_clean$Visitors_2022}<br/>",
+  "<b> Description: </b> {joined_clean$Description}<br/>") |>
+  lapply(htmltools::HTML)
+
+# need to make visitors plot
 GET("https://query.data.world/s/fr5k6pcrcweo7wr657vchbeq6f3cci?dws=00000", write_disk(tf <- tempfile(fileext = ".xlsx")))
 df <- read_excel(tf)
+df2 <- df |> filter(YearRaw >= "2000")
+just_years <- df2 |> filter(YearRaw != "Total")
+national_parks <- df2 |> filter(`Unit Type` == "National Park")
+national_parks2 <- national_parks |> filter(YearRaw != "Total")
+national_parks3 <- national_parks |> filter(YearRaw != "Total") |> filter(Visitors >= 1000000)
+
+# need to make visitors page input options 
 years <- unique(df$YearRaw) 
 type <- unique(df$`Unit Type`)
-national_parks3 <- national_parks |> filter(YearRaw != "Total") |> filter(Visitors >= 1000000)
+
+
 
 ui <- navbarPage("NavBar!",
                  tabPanel("Map",
@@ -39,15 +87,18 @@ ui <- navbarPage("NavBar!",
                                           max = 2016,
                                           value = c(2000, 2016)),
                               # need to find a way to get rid of commas so it looks like years
-                              selectInput("type_select", label = "Select Type to Graph",
-                                          choices = type, multiple = TRUE),
-                              sliderInput("min_visitors", label = "Set a Minimum Amount of Visitors",
+                              selectInput("type_select", 
+                                          label = "Select Type to Graph",
+                                          choices = type,
+                                          selected = NULL),
+                              sliderInput("min_visitors", 
+                                          label = "Set a Minimum Amount of Visitors",
                                           min = 0,
                                           max = 3000000,
                                           value = 1000000),
                             ),
                               mainPanel(
-                                plotOutput("visitors_line")
+                                plotlyOutput("visitors_line")
                         
                               )
                             )
@@ -75,13 +126,15 @@ server <- function(input, output) {
         
     })
     
-    output$visitors_line <- renderPlot({
-      ggplot(data = national_parks3, aes(x = YearRaw, y = Visitors, group = `Unit Name`)) +
-        geom_line(aes(colour = `Unit Name`)) +
+    
+    output$visitors_line <- renderPlotly({
+      plot3 <- ggplot(data = df, aes(x = YearRaw, y = Visitors, group = .data[[input$type_select]])) +
+        geom_line(aes(colour = .data[[input$type_select]])) +
         scale_color_viridis_d() +
         scale_y_continuous(labels = scales::comma) +
         labs(title = "Visitors in National Parks from 2000 to 2016", subtitle = "Minimum visitors is set at 1,000,000") +
         theme_minimal()
+      ggplotly(plot3)
      
     })
     
