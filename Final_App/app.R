c7301d046fdd203903c3c92b495821f4f4215245
Denali Stevens <denalistevens01@gmail.com>
2024-04-28 20:00:56 -0400

    Got the data tables to work the way I wanted using if statments in the reactive statement

--- Final_App/app.R
+++ Final_App/app.R
@@ -62,7 +62,8 @@ label_text <- glue(
   "<b> Visitors in 2022: </b> {joined_clean$Visitors_2022}<br/>",
   "<b> Description: </b> {joined_clean$Description}<br/>") |>
   lapply(htmltools::HTML)
-
+# save rds
+# saves r object that can be manipulated else where
 
 
 # need to make visitors plot
@@ -90,7 +91,14 @@ gsub("\\[.*?\\]", "", joined_display$Established)
 Just_National_parks <- joined_display |> mutate(Date = gsub("\\[.*?\\]", "", joined_display$Established)) |> 
   mutate(Established_D = mdy(Date)) |> 
   select(Name, State, Established_D, Area, `Visitors in 2022`, Description) |> 
-  rename(Established = Established_D) |> st_drop_geometry()
+  rename(Established = Established_D) |> 
+  st_drop_geometry()
+
+np_states <- unique(Just_National_parks$State) |>
+  fct_expand("ALL") |> levels() |>
+  sort()
+
+
 little_states <- unique(Just_National_parks$State) |> sort() 
 
 park_names <- unique(just_np$Name) |> sort()
@@ -103,17 +111,17 @@ parameter_tabs <- tabsetPanel(
   tabPanel("All_Parks",
            selectizeInput("Unit_types", "Select Classification(s) of Park",
                           choices = big_parks,
-                          selected = c("National Park", "National Preserve"),
+                          selected = NULL,
                           multiple = TRUE),
            selectizeInput("State", "Select State(s)",
                           choices = big_states,
-                          selected = "AK",
+                          selected = NULL,
                           multiple = TRUE)
   ),
   tabPanel("Just_National_parks", 
            selectInput("States", "Select State(s)",
-                       choices = little_states,
-                       selected = "AK",
+                       choices = np_states,
+                       selected = NULL,
                        multiple = TRUE)
   )
 )
@@ -122,24 +130,24 @@ parameter_tabs <- tabsetPanel(
 ui <- navbarPage("",
                  tabPanel("Map",
                           fluidPage(
-                          h1("National Park Map"),
-                          hr(strong("This map includes national parks along with historic sites, trails, recreational areas, 
+                            h1("National Park Map"),
+                            hr(strong("This map includes national parks along with historic sites, trails, recreational areas, 
                              and other designations created by the National Park Service.")),
-                          br("Any area highlighted in green is classified as a National Park and more information is 
+                            br("Any area highlighted in green is classified as a National Park and more information is 
                              avaible when clicked on."),
-                          
-                          leafletOutput("map1"),
-                          fluidRow(
                             
-                            column(4,
-                                  selectInput("datasets", "Select Which Dataset to View",
-                                                choices = c("All_Parks", "Just_National_parks"))
-                                  ),
-                            parameter_tabs
+                            leafletOutput("map1"),
+                            fluidRow(
+                              
+                              column(4,
+                                     selectInput("datasets", "Select Which Dataset to View",
+                                                 choices = c("All_Parks", "Just_National_parks"))
+                              ),
+                              parameter_tabs
+                              
+                            ),
                             
-                          ),
-                          
-                          DT::dataTableOutput("table1")
+                            DT::dataTableOutput("table1")
                           )),
                  tabPanel("Visitors Plot",
                           h1("Number of Visitors from 1904 - 2016"),
@@ -170,96 +178,133 @@ ui <- navbarPage("",
 
 
 server <- function(input, output, session) {
-
-    output$map1 <- renderLeaflet({
-      leaflet(nps) |>
-        setView(lng = -110, lat = 39.833, zoom = 2.5) |>
-        addTiles() |>
-        addProviderTiles(providers$OpenStreetMap.Mapnik) |>
-         ## too much for a shiny app 
-       #addPolygons(color = "#09008a",
-                    #weight = .5,
-                    #smoothFactor = .5,
-                    #opacity = 1.0, 
-                    #label = nps$UNIT_NAME) |>
-        addPolygons(color = "#006400",
-                    weight = 1.5,
-                    smoothFactor = .5,
-                    opacity = 1.0, 
-                    fillOpacity = 0.5,
-                    highlightOptions = highlightOptions(color = "#003b00", weight = 2,
-                                                        bringToFront = TRUE),
-                    label = joined_clean$UNIT_NAME,
-                    popup = label_text,
-                    data = joined_clean)
-    })
-    
-    observeEvent(input$datasets, {
-      updateTabsetPanel(inputId = "params", selected = input$datasets)
-    }) 
-    
-    sample <- reactive({
-      switch(input$datasets,
-             All_Parks = All_Parks |> filter(UNIT_TYPE %in% input$Unit_types) |> filter(STATE %in% input$State), 
-             Just_National_parks = Just_National_parks |> filter(State %in% input$States)
-      )
-    })
   
+  output$map1 <- renderLeaflet({
+    leaflet(nps) |>
+      setView(lng = -110, lat = 39.833, zoom = 2.5) |>
+      addTiles() |>
+      addProviderTiles(providers$OpenStreetMap.Mapnik) |>
+      ## too much for a shiny app 
+      addPolygons(color = "#09008a",
+                  weight = .5,
+                  smoothFactor = .5,
+                  opacity = 1.0, 
+                  label = nps$UNIT_NAME) |>
+      addPolygons(color = "#006400",
+                  weight = 1.5,
+                  smoothFactor = .5,
+                  opacity = 1.0, 
+                  fillOpacity = 0.5,
+                  highlightOptions = highlightOptions(color = "#003b00", weight = 2,
+                                                      bringToFront = TRUE),
+                  label = joined_clean$UNIT_NAME,
+                  popup = label_text,
+                  data = joined_clean)
+  })
+  
+  observeEvent(input$datasets, {
+    updateTabsetPanel(inputId = "params", selected = input$datasets)
+  }) 
+  
+  
+  sample <- reactive({
+    switch(input$datasets,
+           All_Parks = 
+             if (length(input$Unit_types) == 0 & length(input$State) == 0) {
+               All_Parks
+             } 
+           
+           else if (length(input$Unit_types) == 0 & length(input$State) != 0) {
+             All_Parks |> 
+               filter(STATE %in% input$State)
+           }
+           
+           else if (length(input$Unit_types) != 0 & length(input$State) == 0) {
+             All_Parks |> 
+               filter(UNIT_TYPE %in% input$Unit_types)
+           } 
+           else {
+             All_Parks |> 
+               filter(UNIT_TYPE %in% input$Unit_types &
+                        STATE %in% input$State)
+           },
+           
+           Just_National_parks = if(length(input$States) == 0){
+             Just_National_parks
+           } else {
+             Just_National_parks |> filter(State %in% input$States)
+           }
+           
+           
+           # Just_National_parks |> filter(State %in% input$States)
+           ## |> filter(
+           # if(length(input$States) == 0){
+           # datatable(sample())
+           # } else {
+           # datatable(sample() |> filter(State %in% input$States))
+           # }
+           # )
+    )
+  })
+  
+  
+  output$table1 <- DT::renderDataTable({
     
     
-    output$table1 <- DT::renderDataTable({
-      datatable(sample())
-    })
+    datatable(sample())
     
     
-    year_visit_reactive <- reactive({
-      just_np_years <- just_np |> 
-        filter(Year != "Total") |> 
-        filter(Year >= input$years_select[1]) |>
-        filter(Year <= input$years_select[2]) |> 
-        filter(Visitors >= input$min_visitors) |>
-        filter(Name %in% input$sel_parks)
-        # If I keep parks to graph I should probably get rid of min_visitors
-        # it doesn't really make sense to have both
-        # Or I can make it so list of parks is limited by the amount of visitors and the years. 
-      just_np_years
-    })
+  })
+  
+  
+  year_visit_reactive <- reactive({
+    just_np_years <- just_np |> 
+      filter(Year != "Total") |> 
+      filter(Year >= input$years_select[1]) |>
+      filter(Year <= input$years_select[2]) |> 
+      filter(Visitors >= input$min_visitors) |>
+      filter(Name %in% input$sel_parks)
+    # If I keep parks to graph I should probably get rid of min_visitors
+    # it doesn't really make sense to have both
+    # Or I can make it so list of parks is limited by the amount of visitors and the years. 
+    just_np_years
+  })
+  
+  observeEvent(input$years_select | input$min_visitors, {
+    park_choices <- just_np |>
+      filter(Visitors >= input$min_visitors) |>
+      filter(Year >= input$years_select[1]) |>
+      filter(Year <= input$years_select[2]) |>
+      select(Name) |>
+      arrange(Name)
     
-    observeEvent(input$years_select | input$min_visitors, {
-      park_choices <- just_np |>
-        filter(Visitors >= input$min_visitors) |>
-        filter(Year >= input$years_select[1]) |>
-        filter(Year <= input$years_select[2]) |>
-        select(Name) |>
-        arrange(Name)
-      
-      updateSelectInput(inputId = "sel_parks",
-                        choices = park_choices)
-      
-      
-    })
+    updateSelectInput(inputId = "sel_parks",
+                      choices = park_choices)
     
-    output$visitors_line <- renderPlotly({
-      validate(
-        need(input$sel_parks, "Please select a park(s)")
-      )
-      plot1 <- ggplot(data = year_visit_reactive(), aes(x = Year, y = Visitors, group = Name,
-                                                        label = Name,
-                                                        label2 = Year,
-                                                        label3 = Visitors)) +
-        geom_line(aes(colour = Name), show.legend = FALSE) +
-        scale_color_viridis_d() +
-        scale_y_continuous(labels = scales::comma) +
-        labs(title = str_glue("Visitors in National Parks from {input$years_select[1]} to {input$years_select[2]}"),
-             caption = ("Minimum Visitors is set at ")) +
-        theme(legend.position = "none")
-      
-      
-      ggplotly(plot1, tooltip = c("label", "label2", "label3"))
-      
-      
-    })
     
+  })
+  
+  output$visitors_line <- renderPlotly({
+    validate(
+      need(input$sel_parks, "Please select a park(s)")
+    )
+    plot1 <- ggplot(data = year_visit_reactive(), aes(x = Year, y = Visitors, group = Name,
+                                                      label = Name,
+                                                      label2 = Year,
+                                                      label3 = Visitors)) +
+      geom_line(aes(colour = Name), show.legend = FALSE) +
+      scale_color_viridis_d() +
+      scale_y_continuous(labels = scales::comma) +
+      labs(title = str_glue("Visitors in National Parks from {input$years_select[1]} to {input$years_select[2]}"),
+           caption = ("Minimum Visitors is set at ")) +
+      theme(legend.position = "none")
+    
+    
+    ggplotly(plot1, tooltip = c("label", "label2", "label3"))
+    
+    
+  })
+  
 }
 
 
